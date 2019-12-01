3df192ccc3f5d668b87a5189f2e0ab050494344c
Matheus Reis <matheus.reisluz@gmail.com>
2019-12-01 00:23:18 -0300

    correcao logica js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,6 +16,10 @@ function plusSlides(n){
     showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slides");
