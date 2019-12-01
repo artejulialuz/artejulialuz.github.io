e466ddf5ff800b10f5b14c6f5648808883242f6b
Matheus Reis <matheus.reisluz@gmail.com>
2019-11-30 23:15:39 -0300

    js items att

--- _site/assets/javascript/main.js
+++ _site/assets/javascript/main.js
@@ -1,1 +1,33 @@
-surtei fodase
+//Abrindo o Modal
+function openModal() {
+    document.getElementById("oModal").style.display = "block";
+}
+
+//Fechando o Modal 
+function closeModal() {
+    document.getElementById("oModal"). style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//Controle Slides
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
